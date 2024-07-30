82f4e2628d50b694456dae506e3b3fe811d573df
siva <kit.24.20bcs030@gmail.com>
2024-07-30 10:28:22 +0530

    first commit

---
+++ home.js
@@ -0,0 +1,6 @@
+const parent=React.createElement('div',{id:"parent"},
+React.createElement('div',{id:'child'},
+[React.createElement('h1',{id:'head'},"Hello siva"),
+React.createElement('h2',{id:'head2'},"Hello prakash")]));
+const root=ReactDOM.createRoot(document.getElementById('root'));
+root.render(parent);
